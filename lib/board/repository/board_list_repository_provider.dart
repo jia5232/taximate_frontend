e4b70a350fc0d46d637abd634fd5ce9c3377361f
jia5232 <jia5232@naver.com>
2024-06-24 14:18:09 +0900

    feat: board list 관련 repository, provider 추가

---
+++ lib/board/repository/board_list_repository_provider.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter/material.dart';
+import 'package:retrofit/http.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:taximate/board/model/board_list_model.dart';
+
+import '../../common/const/data.dart';
+import '../../common/model/cursor_pagination_model.dart';
+import '../../common/provider/dio_provider.dart';
+
+part 'board_list_repository_provider.g.dart';
+
+final boardListRepositoryProvider = Provider<BoardListRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+  final repository = BoardListRepository(dio, baseUrl: "http://$ip/posts/joined");
+  return repository;
+});
+
+@RestApi()
+abstract class BoardListRepository {
+  factory BoardListRepository(Dio dio, {String baseUrl}) = _BoardListRepository;
+
+  @GET('/my')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPaginationModel<BoardListModel>> paginate(
+      @Query('lastPostId') int lastPostId,
+      );
+}
